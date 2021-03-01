818798d770915abfd45e14264aee13d64f895fb8
Kshitij Anand <kshitijanand2@gmail.com>
2021-03-01 22:07:46 +0530

    Minor changes

--- app.js
+++ app.js
@@ -7,5 +7,18 @@ const ejs = require('ejs');
 
 const app = express();
 
+//for able to access static folders like css
+app.use(express.static("public"));
+//for setting up ejs
+app.set('view engine' , 'ejs');
 
-app.use()
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+
+app.listen(3000, function(){
+
+  console.log("Server started on port 3000");
+})
