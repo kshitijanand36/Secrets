d5937d95877d2973737f5dd26ebb7ed48239774b
Kshitij Anand <kshitijanand2@gmail.com>
2021-03-02 15:18:07 +0530

    Minor changes

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ const app = express();
 
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb+srv://admin-kshitij:Test123@cluster0.cdg7o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,{
+  useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+} )
+
+const userSchema = {
+  email : String,
+  password : String
+}
+
+const User = mongoose.model('User' , userSchema);
+
+
+
 //for able to access static folders like css
 app.use(express.static("public"));
 //for setting up ejs
@@ -37,13 +52,49 @@ app.get("/register" , function(req , res){
 
 app.post("/register" , function(req , res){
 
-  res.send("ok!");
+  const new_user = new User({
+    email : req.body.username,
+    password : req.body.password
+  });
+
+  new_user.save(function(err){
+
+    if(err){
+      console.log(err);
+    }
+
+    else{
+      res.render('secrets');
+    }
+  })
 
 });
 
 app.post("/login" , function(req , res){
-res.send("ok!");
 
+  const curr_email = req.body.username;
+  const curr_password = req.body.password;
+
+  User.findOne({email : curr_email} , function(err , result){
+
+    if(err || result == null){
+
+      res.send("Invalid!");
+    }
+
+    else{
+
+      if(result.password == curr_password){
+
+        res.render("secrets");
+      }
+
+      else{
+
+        res.send("Invalid");
+      }
+    }
+  })
 })
 
 app.listen(3000, function(){
